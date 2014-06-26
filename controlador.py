368aae9fc22c40165e8444f9df389441769cc1d6
joseluisnaranjo <joseluisnaranjo@hotmail.es>
2014-06-26 16:36:39 -0700

    se modifico para el ip spoofing

--- controlador.py
+++ controlador.py
@@ -16,6 +16,7 @@ from pyretic.lib.std import *
 from pyretic.lib.query import *
 from ConfigParser import ConfigParser
 import os 
+import time
 
 class ControladorHoneynet(DynamicPolicy):
 	config = ConfigParser()
@@ -32,6 +33,7 @@ class ControladorHoneynet(DynamicPolicy):
 		self.query = packets()
 		self.remotes_ip = {}
 		self.IpPuerto = {}
+		self.IpMac = {}
 		self.query.register_callback(self.paquete)
 		self.network = None
 		super(ControladorHoneynet,self).__init__(self.query)
@@ -55,31 +57,42 @@ class ControladorHoneynet(DynamicPolicy):
 		
 		#Se determinara si el paquete recibido, es o no del tipo ARP
 		if  tipoo == 2054:
-			arp.ejecutarARP(pkt,self.network, self.IpPuerto)
+			arp.ejecutarARP(pkt,self.network, self.IpPuerto, self.IpMac)
 
-        #Se determinara si el paquete recibido, es o no del tipo IP
+        	#Se determinara si el paquete recibido, es o no del tipo IP
 		elif tipoo == 2048:
-			if opcode == 1:
-				#paquete ICMP
-				print "Se ha recibido un paquete ICMP"
-				#con el comando ping y un grep vamos a obtener la direccion de broadcasr del interface de red
-				ipBcast = "ifconfig eth0 | grep 'Bcast'| cut -d':' -f3 | cut -d' ' -f1"
-				broadacast_IP = os.system(ipBcast)
-				#se hace una comparacion con la direccion de destino si es la de broadcast para decidir a donde enviar el paquete 
-				if broadacast_IP == dstip:
-					#destino la honeynet
-					enviar.enviar_paquete(pkt,self.network,self.IpPuerto[dstip])
-				else:
-					#destino la red real
-					enviar.enviar_paquete(pkt,self.network,self.IpPuerto[dstip])
+			#A continuacion se hace una comprobacion de la ip y el puerto de origen con los datos obtenidos al hacer el ARP 
+			if self.IpMac[srcip] == srcmac && self.IpPuerto[srcip]==inport:
 
-			elif opcode == 6:
-				#paquete TCP
-				Sync_Flood.Sync_Flood(pkt,self.network, self.IpPuerto)
+				if opcode == 1:
+					#paquete ICMP
+					print "Se ha recibido un paquete ICMP"
+					#con el comando ping y un grep vamos a obtener la direccion de broadcasr del interface de red
+					ipBcast = "ifconfig eth0 | grep 'Bcast'| cut -d':' -f3 | cut -d' ' -f1"
+					broadacast_IP = os.system(ipBcast)
+					#se hace una comparacion con la direccion de destino si es la de broadcast para decidir a donde enviar el paquete 
+					if broadacast_IP == dstip:
+						#destino la honeynet
+						enviar.enviar_paquete(pkt,self.network,self.IpPuerto[dstip])
+					else:
+						#destino la red real
+						enviar.enviar_paquete(pkt,self.network,self.IpPuerto[dstip])
+
+				elif opcode == 6:
+					#paquete TCP
+					Sync_Flood.Sync_Flood(pkt,self.network, self.IpPuerto)
+
+				else:
+					#Cualquier otro tipo de paquete que se recibano no se analiza en este proyecto por lo que se envia el paquete sin niguna restriccion.
+					enviar.enviar_paquete(pkt,self.network,self.IpPuerto[dstip])
 
 			else:
-				#Cualquier otro tipo de paquete que se recibano no se analiza en este proyecto por lo que se envia el paquete sin niguna restriccion.
-				enviar.enviar_paquete(pkt,self.network,self.IpPuerto[dstip])
+				enviar_paquete(pkt,self.network,self.IpPuerto[dstip])
+		
+		#Se determinara si el paquete recibido, es o no del tipo RARP
+		elif tipoo == 32821:
+			#paquete RARP
+			time.sleep(0.99)
 
 
 def main():
