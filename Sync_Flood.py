e7a124196a5a042d172b3f571c1134d444cd7875
joseluisnaranjo <joseluisnaranjo@hotmail.es>
2014-07-03 11:13:34 -0700

    correccion de errores

--- Sync_Flood.py
+++ Sync_Flood.py
@@ -12,6 +12,7 @@ import enviar
 from pyretic.lib.corelib import *
 from pyretic.lib.std import *
 from pyretic.lib.query import *
+from ConfigParser import ConfigParser
 
 config = ConfigParser()
 config.read("honeynet.cfg") #Se ha creado una instancia de la clase ConfigParser que nos permite  leer un archivo de configuracion
@@ -19,12 +20,12 @@ ListaSolicitudes = []
 ListaAtacantes = []
 ListaClientes = []
 
-def sync_Flood(pkt, network, IpPuerto):
-    switch = pkt['switch']
-    inport = pkt['inport']
-    srcip  = pkt['srcip']        
-    dstip  = pkt['dstip']        
-    opcode = pkt['protocol']
+def Sync_Flood(pkt, network, IpPuerto):
+	switch = pkt['switch']
+	inport = pkt['inport']
+	srcip  = pkt['srcip']        
+	dstip  = pkt['dstip']        
+	opcode = pkt['protocol']
 	
 	#paquete TCP
 	print "Se ha recibido un paquete TCP"
@@ -35,13 +36,13 @@ def sync_Flood(pkt, network, IpPuerto):
 	#A continuacion se  extrae alguas bandetas de TCP, aquellas que nos indican si es syn, syn-ack y ack 
 	tcp_flags = of_payload[94:96]
 	#Se obtiene  de un archivo de configuracion la ip del servidor 
-	ipServidor = self.config.get("SYNFLOOD","ipServidor")
+	ipServidor = config.get("SYNFLOOD","ipServidor")
 	#A continuacion se verifica si se trata de una peticion (SYN)
 	if tcp_flags == "02":
 		#A continuacion se obtiene el tamano de la lista de solicitudes para limitar el numero de solicitudes permitidas
-		tamano_actual_listasolicitudes = len(self.ListaSolicitudes)
-		#Se obtiene  de un archivo de configuracion el tamaño maximo que puede tener la lista de Solicitudes de conexion TCP que ha recibido el servidor 
-		tamano_max_listasolicitudes = self.config.get("SYNFLOOD","tamano")
+		tamano_actual_listasolicitudes = len(ListaSolicitudes)
+		#Se obtiene  de un archivo de configuracion el tamano maximo que puede tener la lista de Solicitudes de conexion TCP que ha recibido el servidor 
+		tamano_max_listasolicitudes = config.get("SYNFLOOD","tamano")
 		#A continuacion se verifica que el tamano de la lsiat sea menor al maximo especificado en el archivo de configuracion
 		if tamano_actual_listasolicitudes < tamano_max_listasolicitudes:
 			#A continuacion se verifica que la direccion IP origen del segmento TCP recibido no sea la del servidor
@@ -51,44 +52,44 @@ def sync_Flood(pkt, network, IpPuerto):
 							
 			else:
 				#A continuacion se verifica que la direccion IP origen del segmento TCP recibido no se encuentre en la lista de solicitudes
-				if srcip in self.ListaSolicitudes:
+				if srcip in ListaSolicitudes:
 					#Si la direccion IP origen del segmento TCP recibido SI se encuentre en la lista de solicitudes, entonces se procede a borrarla de dicha lista y,
-					self.ListaSolicitudes.remove(srcip)
+					ListaSolicitudes.remove(srcip)
 					# Se la agrega a la lista de Atacantes, puesto que se presume  esta intentando hacer un segundo intento de conexion al servidor al mismo timpo..
-					self.ListaAtacantes.append(srcip)
-					#Sin embargo se deja pasar un solo paquete paga obtener estadisticas...
-					Print "Revisar siguiente linea ... no se debe enviar todos los paquetes..."
+					ListaAtacantes.append(srcip)
+					#Sin embargo se deja pasar un solo paquete paga obtener estadisticas
+					print "Revisar siguiente linea no se debe enviar todos los paquetes..."
 					enviar.enviar_paquete(pkt,network,IpPuerto[dstip])								
 																					
 				else:
 					#A continuacion se verifica que la direccion IP origen del segmento TCP recibido  se encuentre en la lista de atacantes
-					if srcip in self.ListaAtacantes:
+					if srcip in ListaAtacantes:
 						#Se envia el paquete a la honeynet 
 						print "Revisar  a donde se envia el paquete!!!"
 						enviar.enviar_paquete(pkt,network,IpPuerto[dstip])		
 
 					else:
 						#A continuacion se verifica que la direccion IP origen del segmento TCP recibido se encuentre en la lista de Clientes lejitimos
-						if srcip in self.ListaClientes:
+						if srcip in ListaClientes:
 							enviar.enviar_paquete(pkt,network,IpPuerto[dstip])									
 
 						else:	
-							#Debido a que no estaba en al lista de clientes lejitimos, se procede a añadirla
-							self.ListaSolicitudes.append(srcip)
-							#Se verifica que efectivamente  se haya añadido a la lista 
-							if self.ListaSolicitudes[-1] == srcip:
+							#Debido a que no estaba en al lista de clientes lejitimos, se procede a anadirla
+							ListaSolicitudes.append(srcip)
+							#Se verifica que efectivamente  se haya anadido a la lista 
+							if ListaSolicitudes[-1] == srcip:
 								enviar.enviar_paquete(pkt,network,IpPuerto[dstip])
 							else:
-								print "Error al añadir  a la lista..."							
+								print "Error al anadir  a la lista..."							
 						
 
 		else:
-			#En caso de que el tamano de la lista sea mayor al tamaño maximo especificado, Se añade a la lista de atacantes la primera solicitud que se agrego a la lista de solicitudes pendientes 
-			self.ListaAtacantes.append(self.ListaSolicitudes[0])
+			#En caso de que el tamano de la lista sea mayor al tamano maximo especificado, Se anade a la lista de atacantes la primera solicitud que se agrego a la lista de solicitudes pendientes 
+			ListaAtacantes.append(ListaSolicitudes[0])
 			# A continuacion se procede a borrar la solicitud que se ha enviado a la lista de atacantes  de la lista de solicitudes para liberar un espacio
-			del self.ListaSolicitudes[0]
+			del ListaSolicitudes[0]
 			# Y finalmente se agrega la nueva solicitud a la lista de solicitudes pendientes 
-			self.ListaSolicitudes.append(srcip)
+			ListaSolicitudes.append(srcip)
 	#A continuacion se verifica si se trata de una respuesta a la peticion (ack) (tercera via de la conexion TCP)
 	elif tcp_flags == "10":					
 		#A continuacion se verifica si la direccion ip origen es la direccion del servidor 
@@ -98,24 +99,24 @@ def sync_Flood(pkt, network, IpPuerto):
 						
 		else:
 			#A continuacion se verifica si la direccion ip de origen se encuentra en la lista de solicitudes
-			if srcip in self.ListaSolicitudes:
+			if srcip in ListaSolicitudes:
 				#Se procede a enviar el paquete, ya que se concidera una conexion legitima 
 				enviar.enviar_paquete(pkt,network,IpPuerto[dstip])
 				#Ya que es una conexion lejitima se procede a eliminar de la lista de solicitudes y
-				self.ListaSolicitudes.remove(srcip)
+				ListaSolicitudes.remove(srcip)
 				#Se le anade a la lista de clientes legitimos
-				self.ListaClientes.append(srcip)
+				ListaClientes.append(srcip)
 				#A continuacion se verifica  si la direccion IP origen  esta en la lista de atacantes aun caundo se trata de uan conexion legitima (lenta) debido a que se revico la tercera via TCP.
-			elif srcip in self.ListaAtacantes:
+			elif srcip in ListaAtacantes:
 				#Se  procede a enviar el paquete 
-				enviar.enviar_paquete(pkt,.network,.IpPuerto[dstip])
+				enviar.enviar_paquete(pkt,network,IpPuerto[dstip])
 				#Se la remueve de la lista de atacantes 
-				self.ListaAtacantes.remove(srcip)
+				ListaAtacantes.remove(srcip)
 				#Se la agrega a la lsita de clientes legitimos.
-				self.ListaClientes.append(srcip)
+				ListaClientes.append(srcip)
 			else:
 				#Se trata de uan conexxion legitima por lo que se envia el paquete sin ninguna restriccion.
-				enviar.enviar_paquete(pkt,.network,IpPuerto[dstip])					
+				enviar.enviar_paquete(pkt,network,IpPuerto[dstip])					
 				
 	else:
 		#Cualquier otra bandera que se envia no se la analiza en este proyecto por lo que se envia el paquete sin niguna restriccion.
