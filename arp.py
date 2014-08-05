f0740de46ec1eac6538701304d2eb30b1a942b1a
joseluisnaranjo <joseluisnaranjo@hotmail.es>
2014-08-05 03:52:17 -0700

    actualizacion

--- arp.py
+++ arp.py
@@ -15,7 +15,7 @@ from pyretic.lib.query import *
 import controlador
 
 
-def ejecutarARP(pkt, network, IpPuerto, IpMac, paqueteARP, IpMacAtacante):
+def ejecutarARP(pkt, network, IpPuerto,puertoHoneynet, IpMac, paqueteARP, IpMacAtacante):
     	
 	inport = pkt['inport']
 	srcip  = pkt['srcip']        
@@ -23,9 +23,10 @@ def ejecutarARP(pkt, network, IpPuerto, IpMac, paqueteARP, IpMacAtacante):
 	srcmac = pkt['srcmac']
 
 	#Se determina si la ip de origen esta en el diccionario IPPuerto
+	#El primer lazo if ayuda unicamente a llenar el diccionario IpPuerto
 	if srcip in IpPuerto:
 		#Si es que ya existe se envia directamente
-		ejecutarEnvio(pkt, network, IpPuerto)
+		tipoARP(pkt, network, IpPuerto[dstip])
 
 	#Si no se encuentra en el diccionario se lo ingresa 
 	else:
@@ -35,33 +36,33 @@ def ejecutarARP(pkt, network, IpPuerto, IpMac, paqueteARP, IpMacAtacante):
 			#Si ya existe esa ip se comrueba que la mac origen corresponde a la almacenada en el diccionario
 			if IpMac[srcip] == srcmac:
 				#Si ya esta registrada se envia el paquete ya que es un cliente fiable
-				ejecutarEnvio(pkt, network, IpPuerto)
+				tipoARP(pkt, network, IpPuerto[dstip])
 			#Si ya no esta registrado podemos dudar si se trata de una atacante 
 			else:
 				#Se guarda el paquete ARP hasta comprobar si el cliente es fiable o no
 				paqueteARP = pkt
 				#Ya que se esta dudando, para comprobar se envia un paquete RARP
-				enviar.enviar_RARP(pkt,network,srcmac)
+				enviar.enviar_RARP(pkt,network)
 
 		#A continuacion se comprueba si esta en el diccionario de atacantes 
 		elif srcip in IpMacAtacante:
 			#Una ves que sabemos que esta en el diccionario de atacantes enviamos a la honeynet
-			ejecutarEnvio(pkt, network, IpPuerto)
+			tipoARP(pkt, network, puertoHoneynet)
 		
 		#Si no se encuentra en el diccionario IpMac quiere decir que es un cliente nuevo
 		else:
 			#Como se trata de un cliente nuevo se incluye en el diccionario y se envia el paquete
 			IpMac[srcip] = srcmac
-			ejecutarEnvio(pkt, network, IpPuerto)
+			tipoARP(pkt, network, IpPuerto[dstip])
 
 
 
-def ejecutarEnvio(pkt, network, IpPuerto):
+def tipoARP(pkt, network, puerto):
 	switch = pkt['switch']
     	opcode = pkt['protocol']
 	inport = pkt['inport']
 	dstip  = pkt['dstip']
-	#Si el paquete ARP recibido, es una solicitud se procede a  reenviarlo por todos los puerto, escepto por elq ue llego.
+	#Si el paquete ARP recibido, es una solicitud se procede a  reenviarlo por todos los puerto, escepto por el que llego.
 	if opcode == 1:
 		for port in network.topology.egress_locations() - {Location(switch,inport)}:
 			puerto = port.port_no					
@@ -70,7 +71,7 @@ def ejecutarEnvio(pkt, network, IpPuerto):
 	# Si el paquete recibido es una respuesta ARP se la enviara unicamente por el puerto en el quese encuentre la Ip destino.
 	elif opcode == 2:			
 		try: 
-			enviar.enviar_paquete(pkt,network,IpPuerto[dstip])
+			enviar.enviar_paquete(pkt,network,puerto)
 					
 		except:
 			print "Error en el envio de la respuesta ARP"
