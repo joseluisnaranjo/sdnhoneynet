62887f8531626b302011188763c6036e8c59d94e
Jose Luis Naranjo Villota <joseluisnaranjo@hotmail.es>
2015-01-23 15:57:09 -0500

    Actualizacion
    Se ha completado la implementacion del metodo enviar, que permite
    inyectar paquetes a la red Openflow

--- enviar.py
+++ enviar.py
@@ -8,17 +8,23 @@
 ###################################################################################
 
 
-import enviar
+
 from pyretic.lib.corelib import *
 from pyretic.lib.std import *
 from pyretic.lib.query import *
 
 
-def enviar_paquete(paquete,network,outport):
-	"""Construct an arp packet from scratch and send"""
-	print "Ejecutando senvio de paquete.."
-	rp = Packet()
-	rp = paquete
-	rp = rp.modify(outport = outport)
-	print rp
-	network.inject_packet(rp)
+def enviar_paquete(paquete,network,sending_port):
+	try:
+		"""Construct an arp packet from scratch and send"""
+		print "Ejecutando senvio de paquete.."
+		rp = Packet()
+		rp = paquete
+		rp = rp.modify(outport = sending_port)
+		print rp
+		network.inject_packet(rp)
+		print "Paquete enviado exitosamente!!..."
+		
+	except:
+		print "Error al enviar el paquete..."
+	
