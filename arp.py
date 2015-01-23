1d2885ca0c82401cce7b4450e0e77fe53096f91c
Jose Luis Naranjo Villota <joseluisnaranjo@hotmail.es>
2015-01-23 15:58:09 -0500

    Actualización
    Se ha terminado la  inplementaciión del modlo arp que contien el metodo
    apra realizar  ARP

--- arp.py
+++ arp.py
@@ -16,16 +16,16 @@ from pyretic.lib.query import *
 
 def ejecutarARP(pkt, network, IpPuerto):
 	switch = pkt['switch']
-        inport = pkt['inport']
-        srcip  = pkt['srcip']        
-        dstip  = pkt['dstip']        
-        opcode = pkt['protocol']
+    inport = pkt['inport']
+    srcip  = pkt['srcip']        
+    dstip  = pkt['dstip']        
+    opcode = pkt['protocol']
 
 	#Se determina si la ip de origen  esta en el diccionario IPPuerto, caso contrario se la agrega.
 	if not srcip in IpPuerto:
 		IpPuerto[srcip] = inport
 
-	#Si el paquete ARP recibido, es una solicitud se procede a  reenviarlo por todos lospuerto, escepto por elq ue llego.
+	#Si el paquete ARP recibido, es una solicitud se procede a  reenviarlo por todos los puerto, escepto por elq ue llego.
 	if opcode == 1:
 		for port in network.topology.egress_locations() - {Location(switch,inport)}:
 			puerto = port.port_no					
@@ -37,9 +37,8 @@ def ejecutarARP(pkt, network, IpPuerto):
 			enviar.enviar_paquete(pkt,network,IpPuerto[dstip])
 					
 	        except:
+				print "Error en el envio de la respuesta ARP"
+	            print pkt
 
-	                print "Error en el envio de la respuesta ARP"
-	                print pkt
-
-					
+    					
 #este es un nuevo cambio para probar gutHub for windows
