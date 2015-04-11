d2d8b1c0936e51b9e7f28280d73b3efe8be8d278
andresman017 <andres_manzano017@hotmail.com>
2015-04-11 10:00:29 -0700

    asd

--- a.py
+++ a.py
@@ -152,3 +152,179 @@ def ejecucion(pkt, network , puertoBloqueado, num):
 
 				num=num+1
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+###################################################################################
+#                        ESCUELA POLITECNICA NACIONAL                             #
+# ------------------------------------------------------------------------------  #
+# Tema: Aplicacion para el controlador pyretic                                    #
+# Programador: Naranjo Villota Jose Luis  (joseluisnaranjo@hotmail.es)            #
+#              Manzano Barrionuevo Andres Michael (andres_manzano017@hotmail.com) #
+# Fecha: Lunes  20 de  Octubre de 2014                                            #
+###################################################################################
+
+from pyretic.lib.corelib import *
+from ConfigParser import ConfigParser
+import enviar
+def thc_ssl_dos(red, pkt, ListaAtacantes, ListaClientes, ListaSolicitudes, IpNumC, IpNumS):
+    config = ConfigParser()
+    config.read("honeynet.cfg")
+    ipServidor = config.get("SYNFLOOD", "ipServidor")
+    num_max_conexiones = config.getint("SYNFLOOD", "num")
+    tamano_max_listasolicitudes = config.getint("SYNFLOOD", "tamano")
+    srcip  = pkt['srcip']
+    dstip  = pkt['dstip']
+
+    ssl_flags1= payload(pkt, 118, 120)
+    ssl_flags2 = payload(pkt, 142, 144)
+    ssl_dato1 = payload(pkt, 108, 110)
+    ssl_dato2 = payload(pkt, 132, 134)
+
+
+
+    print" SSL desde:" +  str (srcip)
+    if (str(ssl_flags1) == "01") or (str(ssl_flags2) == "01"):
+        if srcip == IPAddr(ipServidor):
+            print"paquete ssl del servidor"
+            enviar.enviar_paquete(pkt,red)
+        else:
+                if srcip in ListaAtacantes:
+                    print "Enviar ala Honeynet...Esta en lista de atacantes"
+                    enviar.enviar_Honeynet(pkt, red)
+
+                else:
+                    if srcip in ListaSolicitudes:
+                        if IpNumS.has_key(srcip):
+                                if IpNumS[srcip] < num_max_conexiones:
+                                    IpNumS[srcip] = IpNumS[srcip] + 1
+                                    print "Enviar a la LAN...Menos de 10 solicitudes"
+                                    enviar.enviar_paquete(pkt, red)
+                                else:
+                                    ListaSolicitudes.remove(srcip)
+                                    ListaAtacantes.append(srcip)
+                                    del IpNumS[srcip]
+                                    print "Enviar ala Honeynet...Mas de 10 solicitudes"
+                                    enviar.enviar_Honeynet(pkt, red)
+                        else:
+                            IpNumS[srcip] = 1
+                            print "Enviar a la LAN...Primera solicitud"
+                            enviar.enviar_paquete(pkt, red)
+
+                    else:
+                        if srcip in ListaClientes:
+                            if IpNumC.has_key(srcip):
+                                if IpNumC[srcip] < num_max_conexiones:
+                                    IpNumC[srcip] = IpNumC[srcip] + 1
+                                    print "Enviar a la LAN...Menos de 10 solicitudes"
+                                    enviar.enviar_paquete(pkt, red)
+                                else:
+                                    ListaClientes.remove(srcip)
+                                    ListaAtacantes.append(srcip)
+                                    del IpNumC[srcip]
+                                    print "Enviar ala Honeynet...Mas de 10 solicitudes"
+                                    enviar.enviar_Honeynet(pkt, red)
+                            else:
+                                IpNumC[srcip] = 1
+                                print "Enviar a la LAN...Primera solicitud"
+                                enviar.enviar_paquete(pkt, red)
+                        else:
+                            if len(ListaSolicitudes) < tamano_max_listasolicitudes:
+                                ListaSolicitudes.append(srcip)
+                                print "Enviar a la LAN...Lista de solicitudes menor al maximo"
+                                enviar.enviar_paquete(pkt, red)
+                            else:
+                                ListaAtacantes.append(ListaSolicitudes[0])
+                                del ListaSolicitudes[0]
+                                ListaSolicitudes.append(srcip)
+                                print "Enviar a la LAN...Lista de solicitudes mayor al maximo"
+                                enviar.enviar_paquete(pkt, red)
+    else:
+                if (str(ssl_dato1) == "17") or (str(ssl_dato2) == "17"):
+                    if dstip in ListaSolicitudes:
+                        ListaSolicitudes.remove(dstip)
+                        ListaClientes.append(dstip)
+                        IpNumS[dstip] = IpNumS[dstip] - 1
+                        print "Enviar a la LAN...sacando de la lista de solicitudes"
+                        enviar.enviar_paquete(pkt, red)
+
+                    else:
+                        if dstip in ListaAtacantes:
+                             ListaAtacantes.remove(dstip)
+                             ListaClientes.append(dstip)
+                             print "Enviar a la LAN...sacando de lsiat de atacantes"
+                             enviar.enviar_paquete(pkt, red)
+
+                        else:
+                            if dstip in ListaClientes:
+                                print "Enviar a la LAN... de la lsita de clientes"
+                                enviar.enviar_paquete(pkt, red)
+                else:
+                    if srcip in ListaAtacantes:
+                        print "Enviar a la Honeynet... datos desconocidos...de la lista de atacantes"
+                        enviar.enviar_Honeynet(pkt,red)
+                    else:
+                        print "Enviar a la LAN...... datos desconocidos...No esta en la lista de atacantes"
+                        enviar.enviar_paquete(pkt, red)
+
+
+def payload(pkt,num1,num2):
+    of_payload_code = pkt['raw']
+    of_payload = of_payload_code.encode("hex")
+    print of_payload
+    return of_payload[num1:num2]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
